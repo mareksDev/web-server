48805a436655b8a7d84658809d5b37a78e37d9db
Marek S <mareks@live.com>
2016-02-24 20:00:34 +0200

    update language

--- server.js
+++ server.js
@@ -13,12 +13,11 @@ var middleware = {
 	}
 };
 
-//app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us');
+	res.send('About Us!');
 })
 
 app.use(express.static(__dirname + '/public'));
